26e37984f73a4c1c3808a84826a39629881c6aa8
EugeneDz <e.dzhevadov@gmail.com>
2018-06-30 14:25:59 +0300

    Init

---
+++ src/containers/Main.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './Main.css';
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="main">
+        Main App
+      </div>
+    );
+  }
+}
+
+export default Main;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Main from './containers/Main';
+import registerServiceWorker from './registerServiceWorker';
+
+import './index.css';
+ReactDOM.render(<Main />, document.getElementById('root'));
+registerServiceWorker();
